e17704b033658190511300c6451da689c4bfe749
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2026-07-03 00:13:15 +0600

    release v2.3.13

--- Formula/gitwig.rb
+++ Formula/gitwig.rb
@@ -1,7 +1,7 @@
 class Gitwig < Formula
   desc "Rust-based Terminal User Interface (TUI) for Git"
   homepage "https://github.com/tareqmy/gitwig"
-  version "2.3.12"
+  version "2.3.13"
 
   if OS.mac?
     if Hardware::CPU.intel?
