649ee9754fccbbe3d97c503d6364d2bc85a931b5
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2026-07-02 23:50:56 +0600

    implemented homebrew tap distribution pipeline

---
+++ Formula/gitwig.rb
@@ -0,0 +1,28 @@
+class Gitwig < Formula
+  desc "Rust-based Terminal User Interface (TUI) for Git"
+  homepage "https://github.com/tareqmy/gitwig"
+  version "2.3.11"
+
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://github.com/tareqmy/gitwig/releases/download/v#{version}/gitwig-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "5f1a9bfa9f8e4b78a9c2a1a8c9e01db99e691238eb76e107bbd1b827e85c2c56" # Replace with actual release hash
+    elsif Hardware::CPU.arm?
+      url "https://github.com/tareqmy/gitwig/releases/download/v#{version}/gitwig-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "9c51a0210214a1a3b90dfcb8d1a108a79cb79b76e12e12e10db56e7e1e69a05b" # Replace with actual release hash
+    end
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/tareqmy/gitwig/releases/download/v#{version}/gitwig-v#{version}-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "ec5610dbf88c812bb33fbc55b6c0852ee6cde827ab21e35fbbd1a10e8d99c4c5" # Replace with actual release hash
+    end
+  end
+
+  def install
+    bin.install "gitwig"
+  end
+
+  test do
+    assert_match "gitwig", shell_output("#{bin}/gitwig --version")
+  end
+end
